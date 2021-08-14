cc14a863830d53f3f9601c1435ff0a7c39eae044
Charles <charles.b.jackson@protonmail.com>
2021-08-14 16:11:09 -0500

    Check for quicklisp and sbcl presence

--- build.lisp
+++ build.lisp
@@ -1,15 +1,17 @@
-(push :yxorp-build *features*)
-
-(require 'yxorp)
-
 (declaim
  (optimize
   (compilation-speed 0)
   (debug 0)
   (safety 2)
-  (space 2)
+  (space 3)
   (speed 3)))
 
+(require :asdf)
+
+(#+quicklisp ql:quickload
+ #-quicklisp require 'yxorp)
+
+#+sbcl
 (sb-ext:save-lisp-and-die
  "cl-yxorp"
  :toplevel #'yxorp::main
