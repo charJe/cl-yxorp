cbd9a2bb8bd3c0f544f33ae793e27e6472d43cbf
Charles <charles.b.jackson@protonmail.com>
2021-08-14 16:10:28 -0500

    Track threads and check stream errors

--- main.lisp
+++ main.lisp
@@ -11,12 +11,13 @@
      (t (c) (format *error-output* "~a~%" c))))
 
 (defun websocket-handler (client server)
-  (bt:make-thread
-   (lambda ()
-     (unwind-protect
-          (forward-stream server client)
-       (close client)))
-   :name "YXORP Server->Client")
+  (track-thread
+   (bt:make-thread
+    (lambda ()
+      (unwind-protect
+           (forward-stream server client)
+        (close client)))
+    :name "YXORP Server->Client"))
   (write-headers server)
   (forward-stream client server))
 
@@ -28,37 +29,36 @@
     (write-body-and-headers body client)))
 
 (defun proxy-handler (client config)
-  (#+yxorp-build ignore-errors #-yxorp-build progn
-   (when (string= "USOCKET Client" (bt:thread-name (bt:current-thread)))
-     (setf (sb-thread:thread-name (bt:current-thread)) "YXORP Client->Server"))
-   (let* ((client (if (ssl-config-p (config-ssl config))
-                      (make-ssl-stream client config)
-                      client))
-          (*headers* (parse-request-headers client))
-          (destination (funcall (config-destinator config))))
-     (when (valid-destination-p destination)
-       (multiple-value-bind (host port) (destination-parts destination)
-         (with-open-stream
-             (server (-> (socket-connect
-                          host port :element-type '(unsigned-byte 8))
-                       socket-stream))
-           (if (websocket-p)
-               (websocket-handler client server)
-               (http-handler client server config))))))))
+  (track-thread (bt:current-thread))
+  (with-socket-handler-case client
+    (let* ((client (if (ssl-config-p (config-ssl config))
+                       (make-ssl-stream client config)
+                       client))
+           (*headers* (parse-request-headers client))
+           (destination (funcall (config-destinator config))))
+      (when (valid-destination-p destination)
+        (multiple-value-bind (host port) (destination-parts destination)
+          (with-open-stream
+              (server (-> (socket-connect
+                           host port :element-type '(unsigned-byte 8))
+                        socket-stream))
+            (if (websocket-p)
+                (websocket-handler client server)
+                (http-handler client server config))))))))
 
 (defun ssl-redirect (client config)
-  (#+yxorp-build ignore-errors #-yxorp-build progn
-   (let* ((*headers* (parse-request-headers client))
-          (destination (-> config config-ssl ssl-config-redirect-to)))
-     (setq *headers*
-           (list (cons :http-version "HTTP/1.1") (cons :status 301) (cons :message "Moved Permanently")
-                 (cons :location
-                       (str:concat
-                        "https://" (first (str:split ":" (header :host) :omit-nulls t))
-                        (when (/= 433 destination)
-                          (format nil ":~a" destination))
-                        (header :uri)))))
-     (write-headers client))))
+  (with-socket-handler-case client
+    (let* ((*headers* (parse-request-headers client))
+           (destination (-> config config-ssl ssl-config-redirect-to)))
+      (setq *headers*
+            (list (cons :http-version "HTTP/1.1") (cons :status 301) (cons :message "Moved Permanently")
+                  (cons :location
+                        (str:concat
+                         "https://" (first (str:split ":" (header :host) :omit-nulls t))
+                         (when (/= 433 destination)
+                           (format nil ":~a" destination))
+                         (header :uri)))))
+      (write-headers client))))
 
 (defun start (config
               &aux (config
@@ -66,13 +66,14 @@
                           ((config-p config) config)
                           (:else (config)))))
   (flet ((server (port handler name)
-           (socket-server
-            usocket:*wildcard-host* port
-            handler (list config)
-            :element-type '(unsigned-byte 8)
-            :multi-threading t
-            :in-new-thread t
-            :name name)))
+           (track-thread
+            (socket-server
+             usocket:*wildcard-host* port
+             handler (list config)
+             :element-type '(unsigned-byte 8)
+             :multi-threading t
+             :in-new-thread t
+             :name name))))
     (some-> config
       config-ssl
       ssl-config-redirect-port
@@ -80,22 +81,18 @@
     (server (config-port config) 'proxy-handler "YXORP Server")))
 
 (defun stop ()
-  (->> (bt:all-threads)
-    (remove-if-not
-     (lambda (thread)
-       (str:starts-with-p "YXORP" (bt:thread-name thread))))
-    (mapc 'bt:destroy-thread)))
+  (map-threads 'bt:destroy-thread))
 
 (defun main (&aux (args (uiop:command-line-arguments)))
   (start (nth 0 args))
   ;; this is done in favor of :in-new-thread nil to properly catch interrupt
   (handler-case
       (bt:join-thread
-       (find "YXORP Server" (bt:all-threads)
+       (find "YXORP Server" (the list (bt:all-threads))
              :key 'bt:thread-name
              :test 'string=))
     (#+sbcl sb-sys:interactive-interrupt
      #+clozure ccl:interrupt-signal-condition
      #+ecl interrupt-signal-condition
      () (uiop:quit))))
-  
+
